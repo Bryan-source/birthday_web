e34d1dda285a97f2711ea61574dca83fd5fa5b41
bryansource <bescobar34@yahoo.com>
2022-07-12 01:46:07 -0500

    Define some styles and create whatssapp component

---
+++ src/components/whatssap/Whats.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Whats(props) {
+    return (
+        <div>
+            <a href="https://api.whatsapp.com/send?phone=+523312900619" class="btn-wsp" target="_blank">
+	            <i class="fa fa-whatsapp icono"></i>
+            </a>
+        </div>
+    );
+}
+
+export default Whats;
