4952cf6f58c49e1a1bbd516d72bb6de0dd47abc4
bryansource <bescobar34@yahoo.com>
2022-07-12 12:44:01 -0500

    Add wps button and neon styles

--- src/components/whatssap/Whats.js
+++ src/components/whatssap/Whats.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Whats.css';
 
 function Whats(props) {
     return (
         <div>
-            <a href="https://api.whatsapp.com/send?phone=+523312900619" class="btn-wsp" target="_blank">
-	            <i class="fa fa-whatsapp icono"></i>
+            <a  href="https://wa.me/523330179351/?text=Confirmo%20asistencia%20seremos%20x%20personas" className="btn-wsp">
+                <i className="fa-brands fa-whatsapp"></i>
             </a>
         </div>
     );
