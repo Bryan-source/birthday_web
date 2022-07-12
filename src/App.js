a453a8b997510c6ac285bca52d8889b5ccfc4d09
bryansource <bescobar34@yahoo.com>
2022-07-12 13:21:36 -0500

    Fix some problems with maps

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         </video>
       </header>
       <div className="maps">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d831.6909078777674!2d-103.48141911548869!3d20.62210952575201!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428abbb0d488c25%3A0x114e79d86b05a6f6!2sAv.%20Paseo%20Natura%20No.%2040%2C%20Coto%20Firenze%2C%20Natura%20Bosque%20Residencial%20lll!5e0!3m2!1ses-419!2smx!4v1657607405289!5m2!1ses-419!2smx" width="920" height="600"  loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <iframe title="Map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d831.6909078777674!2d-103.48141911548869!3d20.62210952575201!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428abbb0d488c25%3A0x114e79d86b05a6f6!2sAv.%20Paseo%20Natura%20No.%2040%2C%20Coto%20Firenze%2C%20Natura%20Bosque%20Residencial%20lll!5e0!3m2!1ses-419!2smx!4v1657607405289!5m2!1ses-419!2smx"   loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
       <footer className="App-footer">
         <img src={foot} className="App-image" alt="logo" />
